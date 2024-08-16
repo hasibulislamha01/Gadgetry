bf62c0b0e161fb00c918366ae3f7fa380facbfbe
hasibulislamha01 <hasibulislamha01@gmail.com>
2024-08-16 21:25:01 +0600

    categorized product displaying functionality added

---
+++ src/Pages/GadgetsPage/Categorization.jsx
@@ -0,0 +1,44 @@
+import Select from 'react-select'
+import PropTypes from 'prop-types'
+
+const Categorization = ({setSelectedCategory}) => {
+
+    const options = [
+        { value: 'All', label: 'All Gadgets' },
+        { value: 'Audio', label: 'Audio' },
+        { value: 'Home', label: 'Home' },
+        { value: 'Accessories', label: 'Accessories' },
+        { value: 'Electronics', label: 'Electronics' },
+        { value: 'Mobile', label: 'Mobile' },
+        { value: 'Cameras', label: 'Cameras' },
+        { value: 'Wearables', label: 'Wearables' },
+        { value: 'Storage', label: 'Storage' },
+        { value: 'Computers', label: 'Computers' },
+        { value: 'Transport', label: 'Transport' },
+        { value: 'Gaming', label: 'Gaming' },
+        { value: 'Health', label: 'Health' },
+    ]
+
+    const defaultOption = { value: 'All', label: 'All Gadgets' }
+
+    const handleChange = (option) => {
+        const selected = option?.value
+        // console.log('changing', option,  setSelectedCategory)
+        setSelectedCategory(selected)
+    }
+
+    return (
+        <Select
+            options={options}
+             className='w-[150px]'
+            defaultValue={defaultOption}
+            onChange={handleChange}
+        ></Select>
+    );
+};
+
+
+Categorization.propTypes = {
+    setSelectedCategory: PropTypes.func
+}
+export default Categorization;
--- src/Pages/GadgetsPage/Gadgets.jsx
+++ src/Pages/GadgetsPage/Gadgets.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import GadgetCard from "./GadgetCard";
 import { IoIosSearch } from "react-icons/io";
+import Categorization from "./Categorization";
 
 const Gadgets = () => {
 
@@ -9,7 +10,8 @@ const Gadgets = () => {
     const serverUrl = import.meta.env.VITE_serverLink
 
     const [search, setSearch] = useState('')
-    console.log(search);
+    const [selectedCategory, setSelectedCategory] = useState('')
+    console.log(search, selectedCategory);
 
     useEffect(() => {
         axios.get(`${serverUrl}/gadgets`)
@@ -28,34 +30,52 @@ const Gadgets = () => {
         e.preventDefault()
         const word = e.target.keyword.value
         // console.log(word, word !== null);
-        if(word !== null){
+        if (word !== null) {
             setSearch(word)
         }
     }
-   
+
+
+    // categorization function
+    let categorizedGadgets = gadgets
+    if(selectedCategory === 'All'){
+        categorizedGadgets = gadgets
+    } else {
+        categorizedGadgets = gadgets.filter(gadget => gadget.category.includes(selectedCategory))
+    }
+
+    console.log(categorizedGadgets);
+
 
     return (
-        <div className="min-h-screen lg:pt-6 pt-4">
+        <div className="px-1 md:px-2 min-h-screen lg:pt-6 pt-4">
             <h1 className="heading">Gadgets</h1>
 
-            {/* search */}
-            <form onSubmit={handleSearch} className="relative w-[300px]">
-                <input
-                    // {...register('search')}
-                    onChange={(e)=> setSearch(e.target.value)}
-                    type="text"
-                    name="keyword"
-                    className="py-2 px-4 bg-slate-100 rounded-full w-full outline-none"
-                    placeholder="Search By Product Name..."
-                />
-                <button type="submit" className="absolute top-[0%] right-0 rounded-r-full hover:rounded-full px-2 h-full w-[40px] cursor-pointer bg-slate-300 hover:text-red-600 hover:bg-slate-900 hover:transition-all hover:duration-1000 flex items-center justify-center">
-                    <IoIosSearch className=" font-bold text-lg" />
-                </button>
-            </form>
+            {/* menues */}
+            <div className="flex flex-col md:flex-row  items-center">
+                <Categorization
+                    setSelectedCategory={setSelectedCategory}
+                ></Categorization>
+
+                {/* search */}
+                <form onSubmit={handleSearch} className="relative w-[300px]">
+                    <input
+                        // {...register('search')}
+                        onChange={(e) => setSearch(e.target.value)}
+                        type="text"
+                        name="keyword"
+                        className="py-2 px-4 bg-slate-100 rounded-full w-full outline-none"
+                        placeholder="Search By Product Name..."
+                    />
+                    <button type="submit" className="absolute top-[0%] right-0 rounded-r-full hover:rounded-full px-2 h-full w-[40px] cursor-pointer bg-slate-300 hover:text-red-600 hover:bg-slate-900 transition-all duration-1000 flex items-center justify-center">
+                        <IoIosSearch className=" font-bold text-lg" />
+                    </button>
+                </form>
+            </div>
 
             <div className="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
                 {
-                    gadgets?.filter(gadget => {
+                    categorizedGadgets?.filter(gadget => {
                         return (
                             search.toLowerCase() === '' ? gadget : gadget.name.toLowerCase().includes(search.toLocaleLowerCase())
                         )
