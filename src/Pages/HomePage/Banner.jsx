4687c4cb66e78fcd303550e3d477f336b4bee932
hasibulislamha01 <hasibulislamha01@gmail.com>
2024-08-17 16:49:30 +0600

    bannar updated

--- src/Pages/HomePage/Banner.jsx
+++ src/Pages/HomePage/Banner.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import bannerImg from '../../assets/bannerImg.png'
 
 const Banner = () => {
@@ -7,6 +8,7 @@ const Banner = () => {
                 <div className='themeHeadingFont flex flex-col items-center lg:items-end md:text-[42px] gap-5'>
                     <h1 className=' text-[#D3D3D3]'>Buy Your Tech from</h1>
                     <h1 className='text-[#FFD700]'>Gadgetry</h1>
+                    <Link to='/gadgets' className='btn px-6 bg-[#2F4F4F] text-[#D3D3D3] hover:bg-slate-800 border-none hover:text-rose-400 transition-all duration-700'>Buy Now</Link>
                 </div>
                 <img src={bannerImg} alt="" />
             </div>
